831c684d4f361d3b28f397b1f23bb768aef5523f
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2023-01-13 15:14:15 +0100

    Customer search by name added & employee creation implemented

--- ds-finance-bank-common/src/main/java/interfaces/BankInterface.java
+++ ds-finance-bank-common/src/main/java/interfaces/BankInterface.java
@@ -1,12 +1,16 @@
 package interfaces;
 
 import dto.CustomerDTO;
+import dto.EmployeeDTO;
 
 import javax.ejb.Remote;
+import java.util.ArrayList;
 
 @Remote
 public interface BankInterface {
 
     CustomerDTO createCustomer(String firstName, String lastname, String address, String password);
     String checkPersonRole();
+    EmployeeDTO createEmployee(String firstName, String lastname, String password);
+    ArrayList<CustomerDTO> searchCustomer(String firstName, String lastName);
 }
--- ds-finance-bank-ejb/src/main/java/net/froihofer/service/BankServiceImpl.java
+++ ds-finance-bank-ejb/src/main/java/net/froihofer/service/BankServiceImpl.java
@@ -1,22 +1,28 @@
 package net.froihofer.service;
 
 import dto.CustomerDTO;
+import dto.EmployeeDTO;
 import interfaces.BankInterface;
+import net.froihofer.persistence.dao.EmployeeDAO;
+import net.froihofer.persistence.entity.Employee;
 import net.froihofer.util.mapper.UserMapper;
 import net.froihofer.persistence.dao.CustomerDAO;
 import net.froihofer.persistence.entity.Customer;
 import net.froihofer.util.jboss.WildflyAuthDBHelper;
 
 import javax.annotation.Resource;
+import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Stateless(name = "BankService")
-@PermitAll
+@DeclareRoles({"customer", "employee"})
 public class BankServiceImpl implements BankInterface {
 
     @Resource
@@ -24,27 +30,56 @@ public class BankServiceImpl implements BankInterface {
 
     @Inject
     private CustomerDAO customerDAO;
+    @Inject
+    private EmployeeDAO employeeDAO;
 
 
     @Override
+    @RolesAllowed({"customer", "employee"})
     public String  checkPersonRole() {
         return (this.ctx.isCallerInRole("customer")) ? "customer" : "employee";
     }
 
     @Override
+    @RolesAllowed("employee")
+    public EmployeeDTO createEmployee(String firstName, String lastname, String password) {
+        Employee employee = employeeDAO.createEmployee(firstName, lastname, password);
+
+        createWildflyUser(String.valueOf(employee.getId()), employee.getPassword(), "employee");
+
+        return UserMapper.employeeToDTO(employee);
+    }
+
+    @Override
+    @RolesAllowed("employee")
     public CustomerDTO createCustomer(String firstName, String lastname, String password, String address) {
         Customer customer = customerDAO.createCustomer(firstName, lastname, password, address);
 
-        createWildflyUser(String.valueOf(customer.getId()), customer.getPassword());
+        createWildflyUser(String.valueOf(customer.getId()), customer.getPassword(), "customer");
 
         return UserMapper.customerToDTO(customer);
     }
 
-    private void createWildflyUser(String id, String password) {
+    @Override
+    @RolesAllowed("employee")
+    public ArrayList<CustomerDTO> searchCustomer(String firstName, String lastName) {
+        ArrayList<Customer> customers = customerDAO.searchCustomer(firstName, lastName);
+        ArrayList<CustomerDTO> customersDTO = new ArrayList<>();
+
+        for (Customer c : customers) {
+            customersDTO.add(UserMapper.customerToDTO(c));
+        }
+
+        return customersDTO;
+    }
+
+
+
+    private void createWildflyUser(String id, String password, String PersonRole) {
         File jbossPath = new File("../");
         WildflyAuthDBHelper wildflyAuthDBHelper = new WildflyAuthDBHelper(jbossPath);
 
-        String[] role = new String[]{"customer"};
+        String[] role = new String[]{PersonRole};
 
         try {
             wildflyAuthDBHelper.addUser(id, password, role);
